b078ad0ab07b3b77e5d4199c7d9ed7c145324c65
yyamini <69568821+yyamini@users.noreply.github.com>
2021-03-22 15:14:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void EventHandler(string a);
+
+    public class Operation
+    {
+        public event EventHandler xyz;
+
+        public void Action(string a)
+        {
+            if (xyz != null)
+            {
+                xyz(a);
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Not Registered");
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void CatchEvent(string s)
+        {
+            Console.WriteLine("Method Calling");
+        }
+
+        static void Main(string[] args)
+        {
+            Operation o = new Operation();
+
+            o.Action("Event Calling");
+            o.xyz += new EventHandler(CatchEvent);
+
+            Console.ReadLine();
+        }
+    }
+}
