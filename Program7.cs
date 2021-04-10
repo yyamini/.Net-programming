7a53314f98ef0df4cf7843fde25456a74a7c3b91
yyamini <69568821+yyamini@users.noreply.github.com>
2021-04-10 12:30:31 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,42 @@
+﻿using class2;
+using System;
+using System.Reflection;
+
+namespace class1
+{
+    class class1
+    {
+        public void greetings(string name)
+        {
+            System.Console.WriteLine("class one says hai" + name);
+        }
+    }
+}
+
+
+namespace class2
+{
+    class class2
+    {
+        public void greetings(string name)
+        {
+            System.Console.WriteLine("class two says hai" + name);
+        }
+    }
+}
+
+
+
+namespace multiclass
+{
+    class multiclass
+    {
+        public static void Main()
+        {
+            class1.class1 c1 = new class1.class1();
+            class2.class2 c2 = new class2.class2();
+            c1.greetings("good afternoon");
+            c2.greetings("welcome");
+        }
+    }
+}
